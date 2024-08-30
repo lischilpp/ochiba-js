cb757092b73a212d29c24225dc6e729a0fefc7d9
Linus Schilpp <linus.schilpp@gmail.com>
2024-08-30 15:16:10 +0200

    add demo loading functionality

--- index.js
+++ index.js
@@ -45,69 +45,23 @@ function animateHeader() {
 
 
 function initCodeEditor() {
+    const demoListElem = document.getElementById('demo-list');
     const codeEditorElem = document.getElementById('code-editor');
     const tabs = document.querySelectorAll('#tabs li');
     const runButtonElem = document.getElementById('run-button');
     const previewFrame = document.getElementById('preview-frame');
 
-    let htmlCode = 
-`<html>
-    <body>
-        <h1 id="h1">OchibaJS is awesome!</h1>
-        <script src="ochiba.js"></script>
-    </body>
-</html>
-`;
-    let cssCode =
-`h1 {
-    color: black;
-    font-family: Tahoma, sans-serif;
-    text-align: center;
-    margin-top: 80px;
-}
-
-h1 .leaf { /* make all leaves invisible first */
-    opacity: 0;
-}
-
-@keyframes example1 {
-    0%   { transform: translateY(50px); opacity: 0; }
-    20%  { transform: translateY(-45px); opacity: 0.2; }
-    40%  { transform: translateY(35px); opacity: 0.4; }
-    60%  { transform: translateY(-20px); opacity: 0.6; }
-    80%  { transform: translateY(5px); opacity: 0.8; }
-    100% { transform: translateY(0); opacity: 1; }
-}
-
-@keyframes example2 {
-    0%   { transform: scale(16); color: brown; opacity: 0; }
-    100% { transform: scale(1); opacity: 1; }
-}
-
-@keyframes example3 {
-    0%   { transform: scale(16) scaleY(0); color: brown; opacity: 0; }
-    50%   { transform: scale(1) scaleY(2); color: brown; opacity: 0.5; }
-    80%   { transform: scale(1); color: brown; opacity: 0.5; }
-    100% { transform: scale(1); opacity: 1; }
-}
-`;
-    let jsCode = 
-`const elem = document.getElementById('h1');
-const oc = new OC(elem);
-oc.animateLeaves({
-delay: 0,
-order: 'linear',
-timing: 'linear',
-leafAnimation: {
-    duration: 1,
-    delay: 0,
-    timing: 'ease',
-    keyframes: 'example1'
-},
-duration: 1
-});
-`;
+    let htmlCode = '';
+    let cssCode = '';
+    let jsCode = '';
     let currentLang = 'js';
+
+    function loadDemo(demo) {
+        htmlCode = demo.htmlCode;
+        cssCode = demo.cssCode;
+        jsCode = demo.jsCode;
+        switchToEditorTab('js');
+    }
 
     function updatePreview() {
         const previewContent = `
@@ -152,6 +106,15 @@ duration: 1
         updatePreview();
     });
 
-    // Initial setup
-    switchToEditorTab(currentLang);
+    demoListElem.innerHTML = ochibaDemos.map((demo, i) => `<li data-index="${i}">${demo.name}</li>`).join('');
+
+    demoListElem.addEventListener('click', (e) => {
+        if (e.target.tagName === 'LI') {
+            const demoIndex = parseInt(e.target.dataset.index);
+            loadDemo(ochibaDemos[demoIndex]);
+            updatePreview();
+        }
+    });
+
+    loadDemo(ochibaDemos[0]);
 }
---
+++ ochiba-demos.js
@@ -0,0 +1,59 @@
+const ochibaDemos = [
+    {
+        name: 'Wave from left',
+        htmlCode:
+`
+<html>
+    <body>
+        <h1 id="h1">OchibaJS is awesome!</h1>
+        <script src="ochiba.js"></script>
+    </body>
+</html>
+`,
+
+        cssCode:
+`h1 {
+    color: black;
+    font-family: Tahoma, sans-serif;
+    text-align: center;
+    margin-top: 80px;
+}
+h1 .leaf { /* make all leaves invisible first */
+    opacity: 0;
+}
+@keyframes wave-from-left {
+    0%   { transform: translateY(50px); opacity: 0; }
+    20%  { transform: translateY(-45px); opacity: 0.2; }
+    40%  { transform: translateY(35px); opacity: 0.4; }
+    60%  { transform: translateY(-20px); opacity: 0.6; }
+    80%  { transform: translateY(5px); opacity: 0.8; }
+    100% { transform: translateY(0); opacity: 1; }
+}
+`,
+        jsCode:
+`const elem = document.getElementById('h1');
+const oc = new OC(elem);
+oc.animateLeaves({
+delay: 0,
+order: 'linear',
+timing: 'linear',
+leafAnimation: {
+    duration: 1,
+    delay: 0,
+    timing: 'ease',
+    keyframes: 'wave-from-left'
+},
+duration: 1
+});
+`
+    }
+]
+
+/*
+@keyframes example3 {
+    0%   { transform: scale(16) scaleY(0); color: brown; opacity: 0; }
+    50%   { transform: scale(1) scaleY(2); color: brown; opacity: 0.5; }
+    80%   { transform: scale(1); color: brown; opacity: 0.5; }
+    100% { transform: scale(1); opacity: 1; }
+}
+*/
